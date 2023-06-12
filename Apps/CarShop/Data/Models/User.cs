4a6771f3cd59007dedab7649427d44ecea73a88a
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-12 18:21:10 +0300

    Add project files.

---
+++ Apps/CarShop/Data/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CarShop.Data.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [MinLength(4), MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(5), MaxLength(20)]
+        public string Password { get; set; }
+
+
+        public bool IsMechanic { get; set; }
+    }
+}
